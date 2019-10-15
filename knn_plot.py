0f58d70c845c8c0ba5f371860fa2744edff93e89
Carroll Vance <cs.vance@icloud.com>
2019-10-15 12:56:45 -0500

    Paralelize KNN plots

---
+++ knn_plot.py
@@ -0,0 +1,157 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import KFold
+from sklearn.decomposition import PCA
+import os
+
+
+def plot_knn(X, y, kf, weights: list, metrics: list, title: str = 'KNN', max_k: int = 10, z_score: bool = True,
+             pca: bool = False, pca_dims: int = 10, savefig: bool = False, show: bool = True):
+
+    color_map = {
+        'l1': 'b',
+        'l2': 'r',
+        'cosine': 'g',
+        'chebyshev': 'y',
+        'huber': 'm',
+        'l2_log': 'c',
+        'log_10': 'k'
+    }
+
+    marker_map = {
+        'uniform': 'o--',
+        'distance': 'o-'
+    }
+
+    legend = []
+
+    plt.figure(dpi=150)
+
+    for weight in weights:
+
+        for metric in metrics:
+
+            px = [x for x in range(1, max_k + 1)]
+            py = np.zeros((len(px),))
+
+            for train_index, test_index in kf.split(X):
+                X_train, X_test = X[train_index].copy(), X[test_index].copy()
+                y_train, y_test = y[train_index].copy(), y[test_index].copy()
+
+                if z_score:
+                    u = np.mean(X_train, axis=0)
+                    o = np.std(X_train, axis=0)
+
+                    X_train -= u
+                    X_train /= o
+
+                    X_test -= u
+                    X_test /= o
+
+                if pca:
+                    pca = PCA(n_components=pca_dims)
+                    pca.fit(X_train)
+                    X_train = pca.transform(X_train)
+                    X_test = pca.transform(X_test)
+
+                for k in range(1, max_k + 1):
+                    neigh = KNeighborsClassifier(n_neighbors=k, metric=metric, weights=weight)
+                    neigh.fit(X_train, y_train)
+                    score = neigh.score(X_test, y_test)
+
+                    py[k - 1] += score
+
+            py = 100 * py / 10
+
+            if type(metric) != str:
+                metric_key = metric.__name__
+            else:
+                metric_key = metric
+
+            plt.plot(px, py, '%s%s' % (color_map[metric_key], marker_map[weight]))
+            legend.append("%s (%s)" % (metric_key, weight))
+
+            for i in range(0, len(px)):
+                plt.annotate("%.1f" % py[i], [px[i], py[i]])
+
+    plt.grid()
+    plt.ylabel('%d-Folds Validation Accuracy' % max_k)
+    plt.xlabel('k')
+    plt.xticks(px)
+    plt.title(title)
+    plt.legend(legend)
+
+    if savefig:
+        plt.savefig(os.path.join('figs', "%s.png" % title.replace(':', '')))
+    elif show:
+        plt.show()
+
+
+def plot_knn_wrapper(kwargs):
+    return plot_knn(**kwargs)
+
+
+if __name__ == '__main__':
+    from multiprocessing import Pool
+    from metrics import huber, log_10, l2_log
+
+    df = pd.read_csv('data/data.csv')
+
+    X_cols = [c for c in df.keys()][1:]
+
+    # Remove useless features
+    JUNK_FEATURES = ['REGION-PIXEL-COUNT',  # STD = 0
+                     ]
+
+    for f in JUNK_FEATURES:
+        X_cols.remove(f)
+
+    X = df[X_cols].values
+    y = df['CLASS'].values
+
+    kf = KFold(n_splits=10, random_state=0, shuffle=True)
+    kf.get_n_splits(X)
+
+    weights = ['uniform', 'distance']
+    metrics = ['chebyshev', 'l1', 'l2', huber, log_10, l2_log]
+
+    jobs = []
+    pool = Pool(12)
+
+    for metric in metrics:
+        jobs.append({
+            'X': X,
+            'y': y,
+            'kf': kf,
+            'weights': ['uniform', 'distance'],
+            'metrics': [metric],
+            'title': 'KNN: %s' % metric if type(metric) == str else metric.__name__,
+            'savefig': True,
+            'show': False
+        })
+
+    jobs.append({
+        'X': X,
+        'y': y,
+        'kf': kf,
+        'weights': ['uniform'],
+        'metrics': metrics,
+        'title': 'KNN: Uniform',
+        'savefig': True,
+        'show': False
+    })
+    jobs.append({
+        'X': X,
+        'y': y,
+        'kf': kf,
+        'weights': ['distance'],
+        'metrics': metrics,
+        'title': 'KNN: Distance',
+        'savefig': True,
+        'show': False
+    })
+
+    pool.map(plot_knn_wrapper, jobs)
+
--- metrics.pyx
+++ metrics.pyx
@@ -1,10 +1,10 @@
-import math
+from libc.math cimport log as log_n
 
 def huber(double[:] x, double[:] y):
 
   cdef int n = x.shape[0]
 
-  cdef double delta = 0.025
+  cdef double delta = 0.0125
   cdef double res = 0
   cdef double res_sum = 0
 
@@ -22,7 +22,8 @@ def l2_log(double[:] x, double[:] y):
 
   cdef int n = x.shape[0]
 
-  cdef double delta = 0.025
+  cdef double delta = 0.0125
+  cdef double delta_bias = (delta**2)/2
   cdef double res = 0
   cdef double res_sum = 0
 
@@ -32,6 +33,19 @@ def l2_log(double[:] x, double[:] y):
     if res <= delta:
       res_sum += (res**2)/2
     else:
-      res_sum += math.log(res*2 + 1) + (delta**2)/2
+      res_sum += log_n(res*2 + 1)/2 + delta_bias
+
+  return res_sum / n
+
+def log_10(double[:] x, double[:] y):
+
+  cdef int n = x.shape[0]
+
+  cdef double res = 0
+  cdef double res_sum = 0
+
+  for i in range(n):
+    res = abs(x[i] - y[i])
+    res_sum += log_n(res + 1)
 
   return res_sum / n
